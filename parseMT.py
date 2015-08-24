920232fa656174568da0a7bec4a46242ac7b6f01
anshulsamar <asamar@stanford.edu>
2015-08-23 23:30:26 -0700

    data cleanup

--- parseMT.py
+++ parseMT.py
@@ -1,9 +1,10 @@
 import string
 
-eng_f = open('txt/enc/enc.txt','w')
-hindi_f = open('txt/dec/dec.txt','w')
+data_path = '/deep/group/speech/asamar/nlp/data/hindi/'
+eng_f = open(data_path + 'txt/enc/enc.txt','w')
+hindi_f = open(data_path + 'txt/dec/dec.txt','w')
 punctuation = [',','.','!','?','|']
-with open('source/hindencorp05.plaintext') as f:
+with open(data_path + 'source/hindencorp05.plaintext') as f:
     for line in f:
         s = line.split('\t')
         eng_sent = string.strip(s[3])
@@ -20,10 +21,14 @@ with open('source/hindencorp05.plaintext') as f:
                 sp = hindi_sent.split(hindi_sent[i])
                 hindi_sent = ' '.join(sp)
 
-        eng_f.write(eng_sent)
-        eng_f.write('\n')
+        eng_len = len(eng_sent.split())
+        if eng_len == 0 or eng_len > 25:
+            continue
+        else:
+            eng_f.write(eng_sent)
+            eng_f.write('\n')
 
-        hindi_f.write(hindi_sent)
-        hindi_f.write('\n')
+            hindi_f.write(hindi_sent)
+            hindi_f.write('\n')
 
 
