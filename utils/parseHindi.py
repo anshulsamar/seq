82496fce0b5c5b38a4f7c4e4ed94bc499c250391
anshulsamar <asamar@stanford.edu>
2015-09-24 23:55:21 -0700

    parsing changes

--- utils/parseHindi.py
+++ utils/parseHindi.py
@@ -1,5 +1,3 @@
-# words like don't are split into don ' t
-
 from random import shuffle
 import random
 import math
@@ -10,7 +8,10 @@ import string
 import pdb
 
 eng_punctuation = string.punctuation + '\xe2\x80\x9c' + '\xe2\x80\x9d'
-hindi_punctuation = ['\xe0\xa5\xa4']
+hindi_punctuation = ['\xe0\xa5\xa4'] #danda
+
+# words like don't are split into don ' t
+
 
 def splitSentence(from_sent):
     split_sent = []
@@ -79,7 +80,7 @@ def addToVocab(vocab,sent):
 def removeOOV(vocab,sent):
     new_sent = ''
     for i in range(0,len(sent)):
-        if vocab.index(sent[i]) < 10000:
+        if sent[i] in vocab and vocab.index(sent[i]) < 10000:
             new_sent = new_sent + sent[i] + ' ' 
         else:
             new_sent = new_sent + '<unk> '
@@ -87,12 +88,15 @@ def removeOOV(vocab,sent):
 
 print sys.argv
 
-if len(sys.argv) == 1:
-    print('python parseHindi.py nameOfSaveDir')
+if len(sys.argv) < 3:
+    print('python parseHindi.py nameOfSaveDir cutOff')
     exit()
 
 data_dir = '/deep/group/speech/asamar/nlp/data/hindi/source/'
 save_dir = sys.argv[1] + '/'
+cutOff = int(sys.argv[2])
+print('Only sentences less than ' + str(cutOff) + ' will be taken from English.')
+print('Only sentences less than ' + str(2*cutOff) + ' will be taken from Hindi.')
 
 if not os.path.exists(save_dir):
     print('Making directory ' + save_dir)
@@ -112,6 +116,7 @@ for line in lines:
         train_f.write(line)
     else:
         test_f.write(line)
+    count = count + 1
 data.close()
 train_f.close()
 test_f.close()
@@ -124,13 +129,29 @@ hindi_vocab = {}
 
 data = open(save_dir + 'ptb.train.txt','r')
 num_lines = 0
+max_eng_length = 0
+max_hindi_length = 0
+corresponding_line = ''
 for line in data:
     orig_line = line.lower().strip()
     split_line = orig_line.split('\t')
     eng_sent = splitSentence(split_line[3])
-    addToVocab(eng_vocab,eng_sent)
     hindi_sent = splitSentence(split_line[4])
-    addToVocab(hindi_vocab,hindi_sent)
+
+    if len(eng_sent) < cutOff and len(hindi_sent) < 2*cutOff:
+        addToVocab(eng_vocab,eng_sent)
+        addToVocab(hindi_vocab,hindi_sent)
+        num_lines = num_lines + 1
+        if len(eng_sent) > max_eng_length:
+            max_eng_length = len(eng_sent)
+        if len(hindi_sent) > max_hindi_length:
+            max_hindi_length = len(hindi_sent)
+            corresponding_line = orig_line
+
+print('Lines below cutoff: ' + str(num_lines))
+print('Max English Length: ' + str(max_eng_length))
+print('Max Hindi Length: ' + str(max_hindi_length))
+print(corresponding_line)
 data.close()
 
 print('Sorting Eng Vocab')
@@ -150,14 +171,16 @@ for a in [['ptb.train.txt','enc_train.txt','dec_train.txt'],['ptb.test.txt','enc
         orig_line = line.lower().strip()
         s = orig_line.split('\t')
         eng_sent = splitSentence(s[3])
-        enc_to.write(removeOOV(eng_vocab,eng_sent).strip() + '\n')
         hindi_sent = splitSentence(s[4])
-        dec_to.write(removeOOV(hindi_vocab,hindi_sent).strip() + ' <eos>\n')
-        count = count + 1
-        if (count % 1000 == 0):
-            print('Finished parsing ' + str(count))
-            sys.stdout.flush()
+        if len(eng_sent) < cutOff and len(hindi_sent) < 2*cutOff:
+            enc_to.write(removeOOV(eng_vocab,eng_sent).strip() + '\n')
+            dec_to.write(removeOOV(hindi_vocab,hindi_sent).strip() + '\n')
+            count = count + 1
+            if (count % 1000 == 0):
+                print('Finished parsing ' + str(count))
+                sys.stdout.flush()
 
     data.close()
     enc_to.close()
     dec_to.close()
+    print('Total parsed: ' + str(count))
